919b40d8da3cc2451bc17ad1ed97874ec9be9c9f
threerapatpitarapang <threerapat.pitarapang@gmail.com>
2023-06-22 15:50:20 +0700

    2

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -3,19 +3,17 @@ import bcrypt from "bcrypt";
 
 import prisma from "@/app/libs/prismadb";
 
-export async function POST(
-  request: Request, 
-) {
+export async function POST(request: Request,) {
   const body = await request.json();
-  const { 
+  const {
     email,
     name,
     password,
-   } = body;
+  } = body;
 
-   const hashedPassword = await bcrypt.hash(password, 12);
+  const hashedPassword = await bcrypt.hash(password, 12);
 
-   const user = await prisma.user.create({
+  const user = await prisma.user.create({
     data: {
       email,
       name,
